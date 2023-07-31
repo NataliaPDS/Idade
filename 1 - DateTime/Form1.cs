94895345fb9a7ec3d1257ee928e0a9c6f2e3cc70
Natália <natpereira9102@gmail.com>
2023-07-31 10:11:51 -0300

    primeiro

---
+++ 1 - DateTime/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1___DateTime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //ex: 30/07/2000 e 31/07/2023
+            DateTime dataNasc = dateTimePicker1.Value;
+            DateTime dataAtual = DateTime.Today;
+
+            //TimeSpan a = (dataNasc - dataAtual);
+            int idade = dataAtual.Year - dataNasc.Year;// 2023 - 2006 //esta dando 0
+            ///DateTime.Compare(dataAtual, dataNasc).ToString();//primeira é anterior a segunda, correto -1
+
+            if (dataNasc.Month > dataAtual.Month || dataNasc.Day > dataAtual.Day) //verificando se passou o mes e o dia
+            {
+                idade--;// se nao passou, vou tirar -1
+            }
+            else if (dataNasc.Month <= dataAtual.Month || dataNasc.Day <= dataAtual.Day) 
+            {
+                idade++;//se ja passou +1 
+            }
+            label1.Text = idade.ToString();//esta dando 1, então parece que o if esta correto
+           // MessageBox.Show(idade.ToString());
+
+        }
+    }
+}
