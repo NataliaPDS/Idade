f795ee1ea9e8cd4e561b6b0ca724b9de9245fe3e
Natália <natpereira9102@gmail.com>
2023-07-31 22:54:21 -0300

    terceiro

--- 1 - DateTime/Form1.cs
+++ 1 - DateTime/Form1.cs
@@ -24,14 +24,30 @@ namespace _1___DateTime
             DateTime dataAtual = DateTime.Today;
 
             //TimeSpan a = (dataNasc - dataAtual);
-            int idade = dataAtual.Year - dataNasc.Year;// 2023 - 2006 //esta dando 0
-            ///DateTime.Compare(dataAtual, dataNasc).ToString();//primeira é anterior a segunda, correto -1
+             int idade = dataAtual.Year - dataNasc.Year;// 2023 - 2006 //esta dando 0
+            int comp = DateTime.Compare(dataAtual, dataNasc);
+          
+            //primeira é anterior a segunda, correto -1
             ///lembrete: tentar usar este compare no if
             /// 
 
+            if (comp < 0) 
+            {
+                idade--;
+            }
+
+           /* if (comp == 0) //ja fez aniversario
+            {
+                idade++;
+            }
+            else if(comp == 1)
+            {
+                idade--;
+            }*/
+            label1.Text = idade.ToString();
 
 
-            if (dataNasc.Month > dataAtual.Month || dataNasc.Day > dataAtual.Day) //verificando se passou o mes e o dia
+            /*if (dataNasc.Month > dataAtual.Month || dataNasc.Day > dataAtual.Day) //verificando se passou o mes e o dia
             {
                 idade--;// se nao passou, vou tirar -1
             }
@@ -40,7 +56,7 @@ namespace _1___DateTime
                 idade++;//se ja passou +1 //duvidas
             }
             label1.Text = idade.ToString();//esta dando 1, então parece que o if esta correto
-           // MessageBox.Show(idade.ToString());
+           // MessageBox.Show(idade.ToString());*/
 
         }
     }
